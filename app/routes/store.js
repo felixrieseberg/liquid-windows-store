b7b13809e7f3cde24a84df4a3de668c44b081dc8
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-07 21:35:41 -0700

    Store Route

---
+++ app/routes/store.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function() {
+        return this.store.find('app').then(function (result) {
+            if (result) {
+                return result.slice(10, 50);
+            } else {
+                return result;
+            }
+        })
+    }
+});
