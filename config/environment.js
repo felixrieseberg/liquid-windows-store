daa7ad9d964ba56449b6a3fddda64426784b3003
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-07 22:25:36 -0700

    FlyTo Animation

--- config/environment.js
+++ config/environment.js
@@ -31,11 +31,11 @@ module.exports = function (environment) {
             'script-src': "'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
             'font-src': "'self' http://fonts.gstatic.com http://i.s-microsoft.com", // Allow fonts to be loaded from http://fonts.gstatic.com
             'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
-            'img-src': "'self'",
+            'img-src': "'self' http://store-images.microsoft.com",
             'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
             'media-src': "'self'"
         };
-        
+
         ENV.FIXTURES = {
             enabled: true
         };
