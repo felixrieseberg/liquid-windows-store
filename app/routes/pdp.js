e70febbbec08b1d590dba0e7d0b39ea798880130
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-07 22:25:26 -0700

    Reset Scroll

--- app/routes/pdp.js
+++ app/routes/pdp.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ResetScroll from '../mixins/reset-scroll';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ResetScroll, {
     model: function(params) {
         console.log('params', params);
         return this.store.find('app', params.app_id);
